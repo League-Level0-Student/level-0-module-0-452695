750282099a114e7d42b22597dd6d1b6242ad8e64
ilabstaff <ilabstaff@172.16.116.79>
2019-08-06 17:58:57 -0700

    checkpoint

---
+++ src/LastSummer.java
@@ -0,0 +1,40 @@
+import javax.swing.JOptionPane;
+
+public class LastSummer {
+public static void main(String[] args) {
+	String input= JOptionPane.showInputDialog("what is your name");
+JOptionPane.showMessageDialog(null,"I know that you went swming last summer "+ input +".Muhaha");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ src/_99_extra/Checkpoint.java
@@ -0,0 +1,13 @@
+package _99_extra;
+
+import javax.swing.JOptionPane;
+
+public class Checkpoint {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("what is your favorit color");
+
+
+
+
+}
+}
